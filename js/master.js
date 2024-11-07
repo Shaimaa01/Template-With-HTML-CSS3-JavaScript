5dd001aea125e4dcf3f2f647adedb5c7f8b4fc63
shaimaa <shisko939@gmail.com>
2024-11-07 18:28:29 +0200

    setting-box

--- js/master.js
+++ js/master.js
@@ -1,3 +1,9 @@
+// Check If There`s Local Storage Color Option
+let mainColors = localStorage.getItem("color_option");
+if (mainColors !== null) {
+  document.documentElement.style.setProperty("--main-color", mainColors);
+}
+
 // Toggle Spin Class On Icon
 document.querySelector(".toggle-settings .fa-gear").onclick = function () {
   // Toggle Class Fa-spin Rotation on Self
@@ -6,6 +12,23 @@ document.querySelector(".toggle-settings .fa-gear").onclick = function () {
   // Toggle Class Open On Main Settings Box
   document.querySelector(".settings-box").classList.toggle("open");
 };
+
+// Switch Colors
+const colorLi = document.querySelectorAll(".colors-list li");
+
+// Loop On All List Items
+colorLi.forEach((li) => {
+  // Click On Every List Items
+  li.addEventListener("click", (e) => {
+    // Set Color On Root
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
+    // Set Color On Local Storage
+    localStorage.setItem("color_option", e.target.dataset.color);
+  });
+});
 
 // Select Landing Page Element
 let landingPage = document.querySelector(".landing-page");
