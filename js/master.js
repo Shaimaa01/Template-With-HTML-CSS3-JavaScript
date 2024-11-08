be4c0f4e77f7a301903e7c5805f793cd96f334f9
shaimaa <shisko939@gmail.com>
2024-11-08 19:32:24 +0200

    about-us

--- js/master.js
+++ js/master.js
@@ -1,7 +1,61 @@
 // Check If There`s Local Storage Color Option
 let mainColors = localStorage.getItem("color_option");
+
 if (mainColors !== null) {
   document.documentElement.style.setProperty("--main-color", mainColors);
+
+  // Remove Active Class From All Childrens
+  document.querySelectorAll(".colors-list li").forEach((element) => {
+    element.classList.remove("active");
+
+    // Add Active Class On Element With Data-color === Local Storage Item
+    if (element.dataset.color === mainColors) {
+      // Add Active Class
+      element.classList.add("active");
+    }
+  });
+}
+
+// Random Background Option
+let backgroundOption = true;
+
+// Variable To Control The Interval
+let backgroundInterval;
+
+// store the url img when click no
+let landingimg;
+
+// Select Landing Page Element
+let landingPage = document.querySelector(".landing-page");
+
+// Check If There`s Local Storage Random Background Item
+let backgroundLocalItem = localStorage.getItem("background-option");
+
+// Chek If Random Background Local Storage Is Not Empty
+if (backgroundLocalItem !== null) {
+  if (backgroundLocalItem === "true") {
+    backgroundOption = true;
+    randomizeImgs();
+  } else {
+    backgroundOption = false;
+
+    // take the last background choosen form localStorage
+    landingPage.style.backgroundImage = localStorage.getItem("last-background");
+  }
+
+  console.log(backgroundLocalItem);
+
+  // Remove Active Class From All Spans
+  document.querySelectorAll(".random-backgrounds span").forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  // Add  Active Class
+  if (backgroundLocalItem === "ture") {
+    document.querySelector(".random-backgrounds .yes").classList.add("active");
+  } else {
+    document.querySelector(".random-backgrounds .no").classList.add("active");
+  }
 }
 
 // Toggle Spin Class On Icon
@@ -25,13 +79,45 @@ colorLi.forEach((li) => {
       "--main-color",
       e.target.dataset.color
     );
+
     // Set Color On Local Storage
     localStorage.setItem("color_option", e.target.dataset.color);
+
+    // Remove Active Class From All Childrens
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    e.target.classList.add("active");
   });
 });
 
-// Select Landing Page Element
-let landingPage = document.querySelector(".landing-page");
+// Switch Background Random Option
+const randomBackEl = document.querySelectorAll(".random-backgrounds span");
+
+// Loop On All spans
+randomBackEl.forEach((span) => {
+  // Click On Every span
+  span.addEventListener("click", (e) => {
+    // Remove Active Class From All span
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    // Add active Class On Self
+    e.target.classList.add("active");
+
+    if (e.target.dataset.background === "yes") {
+      backgroundOption = true;
+      randomizeImgs();
+      localStorage.setItem("background-option", true);
+    } else {
+      backgroundOption = false;
+      clearInterval(backgroundInterval);
+      localStorage.setItem("background-option", false);
+    }
+  });
+});
 
 // Get Array Of Imges
 let imgesArray = [
@@ -43,11 +129,22 @@ let imgesArray = [
   "co-5.jpg",
 ];
 
-setInterval(function () {
-  // Get Random Number
-  let randomNumber = Math.floor(Math.random() * imgesArray.length);
+// Function To Randomiz Imgs
+function randomizeImgs() {
+  if (backgroundOption === true) {
+    backgroundInterval = setInterval(function () {
+      // Get Random Number
+      let randomNumber = Math.floor(Math.random() * imgesArray.length);
 
-  // Change Background Image Url
-  landingPage.style.backgroundImage =
-    'url("imgs/' + imgesArray[randomNumber] + '")';
-}, 10000);
+      // Change Background Image Url
+      landingPage.style.backgroundImage =
+        'url("imgs/' + imgesArray[randomNumber] + '")';
+
+      // save the choosen background in localStorage
+      localStorage.setItem(
+        "last-background",
+        landingPage.style.backgroundImage
+      );
+    }, 3000);
+  }
+}
