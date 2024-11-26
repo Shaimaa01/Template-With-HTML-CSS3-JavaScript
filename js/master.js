7b7096fe271f838cf85b965eddf776c7ee4464db
shaimaa <shisko939@gmail.com>
2024-11-26 20:40:30 +0200

    Gallery

--- js/master.js
+++ js/master.js
@@ -148,3 +148,32 @@ function randomizeImgs() {
     }, 3000);
   }
 }
+
+randomizeImgs();
+
+// Select Skills Selector
+let ourSkills = document.querySelector(".skills");
+
+window.onscroll = function () {
+  //Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+
+  // Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  // Window ScrollTop
+  let windowScrollTop = this.scrollY;
+
+  if (windowScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    let allSkills = document.querySelectorAll(
+      ".skill-box .skill-progress span"
+    );
+
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+};
