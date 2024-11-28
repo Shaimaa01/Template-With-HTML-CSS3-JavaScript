fdfae6d689a8d8c4adbbe4fab11bccd5ccb518a3
shaimaa <shisko939@gmail.com>
2024-11-28 22:11:44 +0200

    features , timeline , testimonials

--- js/master.js
+++ js/master.js
@@ -177,3 +177,77 @@ window.onscroll = function () {
     });
   }
 };
+
+// Create Poup With The Image
+let ourGallery = document.querySelectorAll(".gallery img");
+
+ourGallery.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    // Create Overlay Element
+    let Overlay = document.createElement("div");
+
+    // Add Class To overlay
+    Overlay.className = "popup-overlay";
+
+    // Append Overly To The Body
+    document.body.appendChild(Overlay);
+
+    // Create The Popup Box
+    let popupBox = document.createElement("div");
+
+    // Add Class To The Popup Box
+    popupBox.className = "popup-box";
+
+    if (img.alt !== null) {
+      // Create Heading
+      let imgHeading = document.createElement("h3");
+
+      // Create Text For Heading
+      let imgText = document.createTextNode(img.alt);
+
+      // Append The Text To The Heading
+      imgHeading.appendChild(imgText);
+
+      //Append The Heading To The Popup Box
+      popupBox.appendChild(imgHeading);
+    }
+
+    // Create The Image
+    let popupImage = document.createElement("img");
+
+    // Set Image Source
+    popupImage.src = img.src;
+
+    // Add Image To Popup Box
+    popupBox.appendChild(popupImage);
+
+    // Append The Poup Box To Body
+    document.body.appendChild(popupBox);
+
+    // Create The Close Span
+    let closeButton = document.createElement("span");
+
+    // Create The Close Button Text
+    let closeButtonText = document.createTextNode("x");
+
+    // Append Text To Close Button
+    closeButton.appendChild(closeButtonText);
+
+    // Append Class To Close Button
+    closeButton.className = "close-button";
+
+    // Add Close Button To The Poup Box
+    popupBox.appendChild(closeButton);
+  });
+});
+
+// Close Popup
+document.addEventListener("click", function (e) {
+  if (e.target.className == "close-button") {
+    // Remove The Current Popup
+    e.target.parentNode.remove();
+
+    // Remove Overlay
+    document.querySelector(".popup-overlay").remove();
+  }
+});
