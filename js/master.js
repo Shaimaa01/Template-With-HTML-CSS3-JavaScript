14f09d3c8df3825d32abf3bfd7391538f87ac1b6
shaimaa <shisko939@gmail.com>
2024-12-02 20:59:05 +0200

    responsive (Landing Page , About Us , Skills , Gallery , Timeline , Features )

--- js/master.js
+++ js/master.js
@@ -326,3 +326,36 @@ document.querySelector(".reset-options").onclick = function () {
   window.location.reload();
 };
 
+// Toggle Menu
+let toggleBtn = document.querySelector(".toggle-menu");
+let tLinks = document.querySelector(".links");
+
+toggleBtn.onclick = function (e) {
+  // Stop Propagation
+  e.stopPropagation();
+
+  // Toggle Class "menu-active" On Button
+  this.classList.toggle("menu-active");
+
+  // Toggle Class "open" On Links
+  tLinks.classList.toggle("open");
+};
+
+// Click Anywhere Outside Menu And Toggle Button
+document.addEventListener("click", (e) => {
+  if (e.target !== toggleBtn && e.target !== tLinks) {
+    // Check If Menu Is Open
+    if (tLinks.classList.contains("open")) {
+      // Toggle Class "menu-active" On Button
+      toggleBtn.classList.toggle("menu-active");
+
+      // Toggle Class "open" On Links
+      tLinks.classList.toggle("open");
+    }
+  }
+});
+
+// Stop Propagation On Menu
+tLinks.onclick = function (e) {
+  e.stopPropagation();
+};
